5247e2fe1ebee5b85bf176d8aed9652df9873089
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2023-06-13 10:59:43 +0900

    init

---
+++ MyfirstApp/MyfirstApp/MyfirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstAppApp.swift
+//  MyfirstApp
+//
+//  Created by Hyun Seo Jung on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
